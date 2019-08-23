b9cdb98f30dd1422e6cb6f292607ddc31ee4fab6
Moritz Fey <moritz.fey@gmail.com>
2019-08-23 20:45:24 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# OpenMensa Lovelace Card
+This custom card diplays information from the [OpenMensa Sensor](https://github.com/Mofeywalker/openmensa-hass-component).
+
+
+|Preview 1|Preview 2|
+|-|-|
+|![Preview 1](images/preview1.png) | ![Preview 2](images/preview2.png)|
---
+++ openmensa-card.js
@@ -0,0 +1,60 @@
+
+class OpenMensaCard extends HTMLElement {
+  constructor() {
+    super();
+    this.userLang = navigator.language || navigator.userLanguage;
+  }
+  set hass(hass) {
+    if (!this.content) {
+      const card = document.createElement('ha-card');
+      card.header = 'OpenMensa';
+      this.content = document.createElement('div');
+      this.content.style.padding = '0 16px 16px';
+      card.appendChild(this.content);
+      this.appendChild(card);
+    }
+    this.content.innerHTML = ``;
+
+    const entityId = this.config.entity;
+    const state = hass.states[entityId].state;
+    if (state === "online") {
+      const mealCategories = hass.states[entityId].attributes.categories;
+      mealCategories.forEach(element => {
+        let catDiv = document.createElement('div');
+        catDiv.style.paddingBottom = '16px';
+        let headline = document.createElement('b');
+        headline.innerHTML = `${element.name}`;
+        catDiv.appendChild(headline);
+
+        element.meals.forEach(meal => {
+          let mealElement = document.createElement('span');
+          mealElement.innerHTML = `</br>${meal.name}`;
+          catDiv.appendChild(mealElement);
+        });
+
+        this.content.appendChild(catDiv);
+      });
+    } else {
+      let noMealsElement = document.createElement('div');
+      if (this.userLang === 'de') {
+        noMealsElement.innerText = `Keine Daten verfügbar`;
+      } else {
+        noMealsElement.innerText = `No data available`;
+      }
+      this.content.appendChild(noMealsElement);
+    }
+  }
+
+  setConfig(config) {
+    if (!config.entity) {
+      throw new Error('You need to define an entity');
+    }
+    this.config = config;
+  }
+
+  getCardSize() {
+    return 1;
+  }
+}
+
+customElements.define('openmensa-card', OpenMensaCard);
